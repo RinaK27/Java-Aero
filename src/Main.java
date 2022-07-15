4c348005ab1008991fd4c92aaebe0b34168db8bf
RinaK27 <103382676+RinaK27@users.noreply.github.com>
2022-07-15 23:11:15 +0400

    Версия 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int price = 3000; // стоимость билета
-        int x = 20; // каждые 20 рублей потраченные на билет = 1 миля
-        int xx = price / x;
-        int miles = (xx); // мили за купленный билет
-        System.out.println(miles + " бонусных миль");
+        int ticketPrice = 3000; // стоимость билета
+        int milePrice = 20; // каждые 20 рублей потраченные на билет = 1 миля
+        int milesBonus = ticketPrice/milePrice;
+        System.out.println(milesBonus + " бонусных миль");
 
     }
 }
