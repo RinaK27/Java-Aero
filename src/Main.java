452034a9775c949253efe805e6d4387bdef76690
RinaK27 <megustarina@gmail.com>
2022-07-18 00:33:08 +0400

    New

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 3000; // стоимость билета
         int milePrice = 20; // каждые 20 рублей потраченные на билет = 1 миля
-        int milesBonus = ticketPrice/milePrice;
+        int milesBonus = ticketPrice / milePrice;
         System.out.println(milesBonus + " бонусных миль");
 
     }
